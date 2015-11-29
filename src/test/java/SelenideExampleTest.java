1a45e7f15dc1e66ece4ceda51761c0cc1421f7cb
Mikhail <haus.blr@gmail.com>
2015-11-29 22:24:00 +0300

    asd Without Selenide

--- src/test/java/SelenideExampleTest.java
+++ src/test/java/SelenideExampleTest.java
@@ -15,57 +15,67 @@ import com.codeborne.selenide.CollectionCondition;
 import com.codeborne.selenide.ElementsCollection;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
+import org.testng.Assert;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 
-public class SelenideExampleTest extends TestBase{
+public class SelenideExampleTest extends TestBase {
     private FactoryHomePage homePage;
     private FactoryRegistrationPage registrationPage;
     private static final String LINKS_FILE = "./src/main/resources/Links.txt";
+
     @BeforeTest
-    public void setup(){
-        registrationPage = open("login",FactoryRegistrationPage.class);
+    public void setup() {
+        registrationPage = open("login", FactoryRegistrationPage.class);
 
     }
 
     @Test
-    public void simpleLoginTest(){
+    public void simpleLoginTest() {
 
         open("/");
         $(By.linkText("Form Authentication")).click();
         $("#username").val("tomsmith");
         $("#password").val("SuperSecretPassword!");
         $("button[type='submit']").click();
-        $("#flash").should(appear,cssClass("success"),text("You logged into a secure area! "));
+        $("#flash").should(appear, cssClass("success"), text("You logged into a secure area! "));
 
     }
+
     @Test
-    public void staticPageLoginTest(){
+    public void staticPageLoginTest() {
         StaticRegistrationPage.login("tomsmith", "SuperSecretPassword!");
         $(StaticRegistrationPage.FLASH).should(appear, cssClass("success"), text("You logged into a secure area!"));
     }
+
     @Test
-    public void staticPageLogoutTest(){
+    public void staticPageLogoutTest() {
         homePage = registrationPage.login("tomsmith", "SuperSecretPassword!");
         registrationPage = homePage.logout();
         registrationPage.flash.should(appear, cssClass("success"), text("You logged out of the secure area!"));
     }
+
     @Test
-    public void factoryPageLoginTest(){
+    public void factoryPageLoginTest() {
         homePage = registrationPage.login("tomsmith", "SuperSecretPassword!");
         homePage.flash.should(appear, cssClass("success"), text("You logged into a secure area!"));
     }
+
     @Test
     public void linksTest() throws IOException {
         open("");
         List<String> links = Helpers.readAllLines(LINKS_FILE);
-//        ElementsCollection linksFromTheInternet = $$(By.tagName("li a"));
-        Object[] linksText = links.toArray();
-       // $$(By.cssSelector("li a")).shouldHave(CollectionCondition.texts(linksText));
+        ElementsCollection linkes = $$(By.cssSelector("li a"));
+        String[] jopa = linkes.getTexts();
+        List<String> ddd = Arrays.asList(jopa);
+        links.removeAll(ddd);
+        System.out.println(links);
+        Assert.assertTrue(links.isEmpty(),"Elements are no equals: " + links.toString());
 
     }
 }
